807bc231e6e5acfa62bb99d28b5a1e0e5bf49145
miqbalfr12 <miqbalfr12@gmail.com>
2024-06-26 11:27:13 +0700

    fix kalo hapus lagi dikantor hapus juga dikantor

--- src/pages/api/pegawai/hapus/[id].js
+++ src/pages/api/pegawai/hapus/[id].js
@@ -7,6 +7,7 @@ export default async function handler(req, res) {
  }
  if (req.method === "DELETE") {
   const data = await deleteData("pegawai", req.query.id);
+  const kantor = await deleteData("kantor", req.query.id);
   const ResponseInit = {status: 200, statusText: "OK"};
   res.status(200).json({...ResponseInit, message: data});
  }
